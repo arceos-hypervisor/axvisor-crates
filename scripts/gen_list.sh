6e89d017c6f14f2172cd2777683ae76dc8cc9544
zjp <jiping_zhou@foxmail.com>
2025-12-25 09:28:10 +0000

    feat: add crate count as a summary

--- scripts/gen_list.sh
+++ scripts/gen_list.sh
@@ -8,43 +8,49 @@ source "$SCRIPT_DIR/crate_list.sh"
 echo '| Crate | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;crates.io&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | Documentation | Description |'
 echo '|----|---|:--:|----|'
 
+count=0
+
 for repo in ${REPOS[@]}; do
   pushd crates/$repo >/dev/null
   branch=""
 
-  cargo metadata --no-deps --format-version 1 |
-    jq -cr '.packages | .[] | "\(.name) \(.manifest_path) \(.description)"' |
-    while read -r crate manifest_path description; do
+  while read -r crate manifest_path description; do
 
-      if is_hidden_crate $crate; then
-        continue
-      fi
+    if is_hidden_crate $crate; then
+      continue
+    fi
 
-      folder=${manifest_path#$PWD/}
-      folder=${folder%Cargo.toml}
-      # folder is empty if the toml is right under the project root
-      if [[ -z $folder ]]; then
-        url="$ROOT/$repo"
-      else
-        : ${branch:=$(curl -s https://api.github.com/repos/$ORG/$repo | jq -r .default_branch)}
-        url="$ROOT/$repo/tree/$branch/$folder"
-      fi
+    count=$((count + 1))
 
-      if curl --output /dev/null --silent --head --fail $(get_crate_url $crate); then
-        # In crates.io
-        crates_io="[![Crates.io](https://img.shields.io/crates/v/$crate)](https://crates.io/crates/$crate)"
-        doc="[![Docs.rs](https://docs.rs/$crate/badge.svg)](https://docs.rs/$crate)"
-      else
-        # Not in crates.io
-        crates_io="N/A"
-        doc_url="https://arceos-hypervisor.github.io/$repo"
-        doc="[![Docs.rs](https://img.shields.io/badge/docs-pages-green)]($doc_url)"
-      fi
+    folder=${manifest_path#$PWD/}
+    folder=${folder%Cargo.toml}
+    # folder is empty if the toml is right under the project root
+    if [[ -z $folder ]]; then
+      url="$ROOT/$repo"
+    else
+      : ${branch:=$(curl -s https://api.github.com/repos/$ORG/$repo | jq -r .default_branch)}
+      url="$ROOT/$repo/tree/$branch/$folder"
+    fi
 
-      description="$description."
-      echo "| [$crate]($url) | $crates_io | $doc | $description |"
+    if curl --output /dev/null --silent --head --fail $(get_crate_url $crate); then
+      # In crates.io
+      crates_io="[![Crates.io](https://img.shields.io/crates/v/$crate)](https://crates.io/crates/$crate)"
+      doc="[![Docs.rs](https://docs.rs/$crate/badge.svg)](https://docs.rs/$crate)"
+    else
+      # Not in crates.io
+      crates_io="N/A"
+      doc_url="https://arceos-hypervisor.github.io/$repo"
+      doc="[![Docs.rs](https://img.shields.io/badge/docs-pages-green)]($doc_url)"
+    fi
 
-    done
+    description="$description."
+    echo "| [$crate]($url) | $crates_io | $doc | $description |"
+
+    # Process substitution to make the count work as expected.
+  done < <(cargo metadata --no-deps --format-version 1 |
+    jq -cr '.packages | .[] | "\(.name) \(.manifest_path) \(.description)"')
 
   popd >/dev/null
 done
+
+printf "\nTotal $count crates from $ORG are available!\n"
